972d12ec2bf155afcfb0c54b4e3bb37c9279cc8a
Matthew Toohey <contact@mtoohey.com>
2023-06-29 20:32:34 -0400

    feat: switch to upstream zmk remote

--- flake.nix
+++ flake.nix
@@ -13,26 +13,26 @@
           (final: prev: {
             zephyr-sdk = final.stdenv.mkDerivation rec {
               pname = "zephyr-sdk";
-              version = "0.13.2";
+              version = "0.15.0";
               nativeBuildInputs = [
-                final.python38
                 final.autoPatchelfHook
+                final.cmake
+                final.python38
+                final.stdenv.cc.cc.lib
+                final.wget
                 final.which
               ];
               src = builtins.fetchurl {
-                url = "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${version}/zephyr-sdk-${version}-linux-x86_64-setup.run";
-                sha256 = "M03Tv7PKaWWbOBriASohiKQUpsht7HXj7sOwsz4RrB4=";
+                url = "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${version}/zephyr-sdk-${version}_linux-x86_64.tar.gz";
+                sha256 = "0ps17zrk6viwr5ikn40ybm1pwgzrpcyyqc0s3chnwzjb23f26g4r";
               };
               dontUnpack = true;
+              dontConfigure = true;
               installPhase = ''
-                # extract outer self-extracting archive
-                cp $src $TEMP/installer.run
-                chmod +x $TEMP/installer.run
-                $TEMP/installer.run --noexec --target $TEMP/outer
-
-                # run the setup tool
-                cd $TEMP/outer
-                bash ./setup.sh -d $out -y -norc -nocmake
+                mkdir $out
+                tar -xf $src -C $out --strip-components=1
+                cd $out
+                bash ./setup.sh -t x86_64-zephyr-elf
               '';
             };
           })
@@ -95,10 +95,10 @@
             export Zephyr_DIR=$PWD/zephyr/share/zephyr-package/cmake
 
             mkdir out
-            west build -s zmk/app -b corne-ish_zen_left -- -DZMK_CONFIG="$PWD/config"
-            mv build/zephyr/zmk.uf2 out/corne-ish_zen_left.uf2
-            west build --pristine -s zmk/app -b corne-ish_zen_right -- -DZMK_CONFIG="$PWD/config"
-            mv build/zephyr/zmk.uf2 out/corne-ish_zen_right.uf2
+            west build -s zmk/app -b corneish_zen_v1_left -- -DZMK_CONFIG="$PWD/config"
+            mv build/zephyr/zmk.uf2 out/corneish_zen_v1_left.uf2
+            west build --pristine -s zmk/app -b corneish_zen_v1_right -- -DZMK_CONFIG="$PWD/config"
+            mv build/zephyr/zmk.uf2 out/corneish_zen_v1_right.uf2
           '';
         installPhase = "cp -r out $out";
       };
--- west-modules.nix
+++ west-modules.nix
@@ -2,67 +2,96 @@
   {
     repo = "zmk";
     path = "zmk";
-    owner = "LOWPROKB";
-    rev = "a5d0e6f98de2cef88f03171b07ab12a13a7e2d1e";
-    sha256 = "9aqBTOcQsN1dA/9KfBjR2MQ2INnxSRsVIY9pCouwTNQ=";
+    owner = "zmkfirmware";
+    rev = "b259d5a22e423458b94bd1aba70fd62f327528c0";
+    sha256 = "uOzO6XLf3VV8+lVY5enOqX5wb9bNlU1awG2B+txKOsg=";
     createDotGit = true;
   }
   {
     repo = "zephyr";
     path = "zephyr";
     owner = "zmkfirmware";
-    rev = "f2108f1646b964adffb9c7ed74641c828fef2a44";
-    sha256 = "jEy+4OdBieu1HnSFpd/aJoFBDI1cTwiyqaXuNesHg4w=";
+    rev = "0a586db7b58269fb08248b081bdc3b43452da5f4";
+    sha256 = "MfXHLXjQ3wPGl3yPbVHDL7RzxyJ+ssAGtNcaInNnFbI=";
     createDotGit = true;
-  }
-  {
-    repo = "uf2";
-    path = "tools/uf2";
-    owner = "microsoft";
-    rev = "162acf4e4cb7c8e9ec589c842d277dcb5c43db89";
-    sha256 = "uxMV7J/Od6pCZMOoiTYBSSV/iw75f0OhlNS2Ah6lRkQ=";
-  }
-  {
-    repo = "cmsis";
-    path = "modules/hal/cmsis";
-    rev = "c3bd2094f92d574377f7af2aec147ae181aa5f8e";
-    sha256 = "SZbopDEcf/Ye6OkrCvC4gn+G0u2MPn1d0RdxzBy9Hj8=";
-  }
-  {
-    repo = "hal_atmel";
-    path = "modules/hal/atmel";
-    rev = "9f0a699a2f0253dc9688211ef696daa7b8fadb75";
-    sha256 = "A9ry772WJuH95Le1GV447/2oA+3E5Pu4VnVLlgDFUuE=";
   }
   {
     repo = "canopennode";
     path = "modules/lib/canopennode";
-    rev = "468d350028a975b96563e58344de48281a0ab371";
-    sha256 = "vXPtAQqVj0nQdhaFiPVg7AIIFypa8p9GAOW9m6anr5U=";
+    rev = "53d3415c14d60f8f4bfca54bfbc5d5a667d7e724";
+    sha256 = "gK+vpOu8vZBFsDYnme7tLckxWrZpfKI+t3txEX/oGe8=";
   }
   {
-    repo = "civetweb";
-    path = "modules/lib/civetweb";
-    rev = "e6903b80c09d17cd1a8bb32e40080005558aad29";
-    sha256 = "M7T3m8xeFME3pAYJa87cF/xqRBZP5wLg8CLtWI8zU4g=";
+    repo = "chre";
+    path = "modules/lib/chre";
+    rev = "ef76d3456db07e4959df555047d6962279528c8d";
+    sha256 = "1fE1AkCCn60Z0bw+ZViQEDhPRj+tKl2zJvd9qlHZVgI=";
   }
   {
-    repo = "hal_espressif";
-    path = "modules/hal/espressif";
-    rev = "895d99ed0902e8f3ddde7bdeb3a1fe2fa875b005";
-    sha256 = "wRVA3ROrzMhBeUFmjuCUl7jeDzA0FnUnCqTcBTGoCYs=";
+    repo = "cmsis";
+    path = "modules/hal/cmsis";
+    rev = "093de61c2a7d12dc9253daf8692f61f793a9254a";
+    sha256 = "MmC20eShVDPsjD3WuW0uekCqya+SRwbkH2+LRct3pAw=";
+  }
+  {
+    repo = "edtt";
+    path = "tools/edtt";
+    rev = "1ea61a390d2bfcf3b2ecdba8f8b0b98dfdffbd11";
+    sha256 = "/Tbyf2R6u307Gx0JQF5L3e2nhnnqiuyXBE3XCLael4E=";
   }
   {
     repo = "fatfs";
     path = "modules/fs/fatfs";
-    rev = "1d1fcc725aa1cb3c32f366e0c53d7490d0fe1109";
-    sha256 = "+Odgcwm7d7ZWUIr3WPXYD/NadZCJnmwKOmifFs9iXEQ=";
+    rev = "a30531af3a95a9a3ea7d771ea8a578ebfed45514";
+    sha256 = "YfzgceyC1vEgPkExZBr1VpsYs2i96vp108acqkIfgxE=";
+  }
+  {
+    repo = "fff";
+    path = "modules/lib/fff";
+    rev = "6ce5ba26486e93d5b7696a3e23f0585932c14b16";
+    sha256 = "/nv168bPJ0DiT/f5wOw2qWQshCKtZ3Xkz7bMFVnUmfM=";
+  }
+  {
+    repo = "hal_altera";
+    path = "modules/hal/altera";
+    rev = "0d225ddd314379b32355a00fb669eacf911e750d";
+    sha256 = "H2C+3ASsC5O/mA+O5EJqfoJgx0BhSN0W3OhWNHVQxRU=";
+  }
+  {
+    repo = "hal_atmel";
+    path = "modules/hal/atmel";
+    rev = "1d237f2e2f262751975b6da6e03af569b2b49b2b";
+    sha256 = "NAwontfqVaXfHRmcYZVNr9XuJDdszpuzPK4pZDFbuNw=";
+  }
+  {
+    repo = "hal_espressif";
+    path = "modules/hal/espressif";
+    rev = "2cb20ac6c5b25f3c91b35e7997451db47030b7cb";
+    sha256 = "PcXml65AbKIvv55NQXNYWklJkOsXS3j7YXhhNEa+WbI=";
+  }
+  {
+    repo = "hal_gigadevice";
+    path = "modules/hal/gigadevice";
+    rev = "dd0e0322474462b58059e6fedaf1d67d2a0864d0";
+    sha256 = "AnpbMe1jmWeih8k82aKgpjtah+oL2GSNcJOn4YJBHVM=";
+  }
+  {
+    repo = "hal_infineon";
+    path = "modules/hal/infineon";
+    rev = "4af06965f57ba1e7d170e6a97d24c33785543a8c";
+    sha256 = "a4fALM4HfLW+iq27i+jYIOZv1ozc0uwtBLIAs+Js+/E=";
+  }
+  {
+    repo = "hal_microchip";
+    path = "modules/hal/microchip";
+    rev = "5d079f1683a00b801373bbbbf5d181d4e33b30d5";
+    sha256 = "CJdNHsowN9WsKSHekypUdeJvOJ19/0PHZU874CXVaoU=";
   }
   {
     repo = "hal_nordic";
     path = "modules/hal/nordic";
-    rev = "f0d54d8449acbee49b3cebcef0e3e56640c50277";
-    sha256 = "i0kIgDO7D5E9/3+CpZ5T8INjSv5+JD4+eKg5E6OZaRY=";
+    rev = "249199ec5a5c31d170659921048764e96d05cc0e";
+    sha256 = "UuxI95572bfr4H1nz9iQ+wkp2L/wlhcof27TESHgOOI=";
   }
   {
     repo = "hal_nuvoton";
@@ -71,58 +100,194 @@
     sha256 = "vp4G1qKfVVxEUJgD0lBy9fO3SNcJ8rnRQcS4fW1UgiQ=";
   }
   {
-    repo = "hal_stm32";
-    path = "modules/hal/stm32";
-    rev = "cc8731dba4fd9c57d7fe8ea6149828b89c2bd635";
-    sha256 = "T05icRoiWo4hGhAYvZl4gkd/zvcgSyVwCfx2u7nXrEQ=";
+    repo = "hal_nxp";
+    path = "modules/hal/nxp";
+    rev = "708c95825b0d5279620935a1356299fff5dfbc6e";
+    sha256 = "n00+N5DEtGT6nFDp/21z432SP0cZKLYp+PjC9NZVZ/k=";
   }
   {
-    repo = "libmetal";
-    path = "modules/hal/libmetal";
-    rev = "9d4ee2c3cfd5f49861939447990f3b7d7bf9bf94";
-    sha256 = "nbIJ578LNRgeVhbz4MeIOCmny23zQIOm6nUxO3yZr8g=";
+    repo = "hal_openisa";
+    path = "modules/hal/openisa";
+    rev = "40d049f69c50b58ea20473bee14cf93f518bf262";
+    sha256 = "ZxAgqVvYFBGl+VFigP9+4MRtf15wXEfvLZxOJNg1a88=";
   }
   {
     repo = "hal_quicklogic";
     path = "modules/hal/quicklogic";
     rev = "b3a66fe6d04d87fd1533a5c8de51d0599fcd08d0";
     sha256 = "XpPROaiZN0KRxQCtEOYaXSEBtxAo+atnH1ytOMXpYUI=";
+    # repo-path: hal_quicklogic
+  }
+  {
+    repo = "hal_renesas";
+    path = "modules/hal/renesas";
+    rev = "468d3f2146d18c7f86a4640fc641cc1d20a4a100";
+    sha256 = "AhCtgVFVOzoI6Im4I0lThEh9qmNkru63aTn87KsIk4M=";
+  }
+  {
+    repo = "hal_rpi_pico";
+    path = "modules/hal/rpi_pico";
+    rev = "a094c060e0c2d43c7f9d8f5c06cc0665117e0c18";
+    sha256 = "yYJAiYjehU99dK6yciQfAbNsDRV8+FWsfZfwUmha5qE=";
+  }
+  {
+    repo = "hal_silabs";
+    path = "modules/hal/silabs";
+    rev = "1ec8dd99aa4ac3e8632d2aa28a7438049bb27102";
+    sha256 = "cAmHatdFSvMmiSeB1i3OTkdBNNk+qQZLq2eEPR6bnAE=";
+  }
+  {
+    repo = "hal_st";
+    path = "modules/hal/st";
+    rev = "cccbc24c14decfd3f93959f7b14514536af973c7";
+    sha256 = "ATuBgdRfVDd2C8sijf2JGdlzi2CfEjQUm3xAwVCKvCU=";
+  }
+  {
+    repo = "hal_stm32";
+    path = "modules/hal/stm32";
+    rev = "642e199c59828137dc6b1c7044a289d4269886d1";
+    sha256 = "KhL+iwJ0OHv4wySeglQkVGuoqx0TCk0F/IoHzDok5dI=";
+  }
+  {
+    repo = "hal_telink";
+    path = "modules/hal/telink";
+    rev = "38573af589173259801ae6c2b34b7d4c9e626746";
+    sha256 = "ioHMx0rpbRppxAoT5yLcIHgLhoO9vyuas9YwaOEyvtQ=";
+    # submodules: true
+  }
+  {
+    repo = "hal_ti";
+    path = "modules/hal/ti";
+    rev = "000b944a788b6005d7776198e1348f5c8a657259";
+    sha256 = "7pjXFoDe4AQvqlshZRPcmv1+kVTFWyVVuRvRAtXPqhE=";
+  }
+  {
+    repo = "hal_wurthelektronik";
+    path = "modules/hal/wurthelektronik";
+    rev = "24ca9873c3d608fad1fea0431836bc8f144c132e";
+    sha256 = "CIoXgrW/G62hFSSj1q+tnlf5AdzEGnJtQQ2fBYgcS2g=";
+  }
+  {
+    repo = "hal_xtensa";
+    path = "modules/hal/xtensa";
+    rev = "63f655362423aa49507da7977a2d37142e8debeb";
+    sha256 = "4sBAcYvEmdlBAVsEflvjgDLNI/kUZ85KUI/V2oKsT14=";
+  }
+  {
+    repo = "libmetal";
+    path = "modules/hal/libmetal";
+    rev = "2f586b4f1276fb075ee145421bdf6cbe5403aa41";
+    sha256 = "hFcyAuNXIiXMlu6piH960nNXoTIzmOyOMKCDorofE+Q=";
+  }
+  {
+    repo = "liblc3";
+    path = "modules/lib/liblc3";
+    rev = "448f3de31f49a838988a162ef1e23a89ddf2d2ed";
+    sha256 = "t0dFjIwOGKK5yBn1Y0XFl5jPxviAnER1wLoAH+YQKR8=";
+  }
+  {
+    repo = "littlefs";
+    path = "modules/fs/littlefs";
+    rev = "ca583fd297ceb48bced3c2548600dc615d67af24";
+    sha256 = "tj03GWzq3WFr+HPs/27ijoTBxascC69WDzLYcm2Wt4M=";
+  }
+  {
+    repo = "loramac-node";
+    path = "modules/lib/loramac-node";
+    rev = "ce57712f3e426bbbb13acaec97b45369f716f43a";
+    sha256 = "Z1pbqYZlCQexAhgC+vMkrTC++OaWZ2DtM5YLMnHJy2E=";
   }
   {
     repo = "lvgl";
     path = "modules/lib/gui/lvgl";
-    rev = "31acbaa36e9e74ab88ac81e3d21e7f1d00a71136";
-    sha256 = "kNkaEYP7Dfkoe65lgO47kaObklN2itEit1yLGhgkP0E=";
+    owner = "zmkfirmware";
+    rev = "70a7849726be8375e3d941153dc417823ea7f355";
+    sha256 = "KD2NMez0vkyIDgBw4Pc8l/A+J4DnE56hXNyKs+b09ZA=";
+  }
+  {
+    repo = "lz4";
+    path = "modules/lib/lz4";
+    rev = "8e303c264fc21c2116dc612658003a22e933124d";
+    sha256 = "zqItRECBLA2poyihoEXwpNxfn7koxxyA5vud8Po7Gs8=";
   }
   {
     repo = "mbedtls";
     path = "modules/crypto/mbedtls";
-    rev = "24d84ecff195fb15c889d9046e44e4804d626c67";
-    sha256 = "Q9sZxP/t5+WziZ/BtcZt59pS4CGr7o2PnPHUV2oCl5A=";
+    rev = "7fed49c9b9f983ad6416986661ef637459723bcb";
+    sha256 = "7vRWNuhCROmPN3rx5csTzACKEnzxJ3hkjw5j/DoplSA=";
   }
   {
     repo = "mcuboot";
     path = "bootloader/mcuboot";
-    rev = "3fc59410b633a6d83bbb534e43aac43160f9bd32";
-    sha256 = "C2jTrcDvmD1qSkJEd3xkv/r/EVzJwJKwYtxh9YnDzEw=";
+    rev = "13f63976bca672ee018f9d55f1e31f02f4135b64";
+    sha256 = "uO9IFWykclfiZ9ZCuONhaj9iCy0XKXuJ6JqwQ+40Cxk=";
+  }
+  {
+    repo = "mipi-sys-t";
+    path = "modules/debug/mipi-sys-t";
+    rev = "0d521d8055f3b2b4842f728b0365d3f0ece9c37f";
+    sha256 = "x0HNf+x/yUYn4gZw/jtweQkuBN/o01a4lE3EvZayRbI=";
+  }
+  {
+    repo = "nanopb";
+    path = "modules/lib/nanopb";
+    rev = "dc4deed54fd4c7e1935e3b6387eedf21bb45dc38";
+    sha256 = "ugBPTMmw9kfd8PDb3FdegVoir28sg544QtRCD4uBv04=";
+  }
+  {
+    repo = "net-tools";
+    path = "tools/net-tools";
+    rev = "e0828aa9629b533644dc96ff6d1295c939bd713c";
+    sha256 = "XMWGwXxEw0KQv8U8wjf50W6WM0PI0fm9lIUiTpCh1VQ=";
+  }
+  {
+    repo = "nrf_hw_models";
+    path = "modules/bsim_hw_models/nrf_hw_models";
+    rev = "65bc5305d432c08e24a3f343006d1e7deaff4908";
+    sha256 = "9YN0BxZXiYrfgfY53XyEZl5TGADfVtaHBI+wL8TEv4s=";
   }
   {
     repo = "open-amp";
     path = "modules/lib/open-amp";
-    rev = "de1b85a13032a2de1d8b6695ae5f800b613e739d";
-    sha256 = "ScbHD7IbqBacTP/YuCDPBvZrZLiTPXu0nVv2IN9HcSA=";
+    rev = "8d53544871e1f300c478224faca6be8384ab0d04";
+    sha256 = "oKgX4ExkeIQ37xv4e+SfZ5/L5TulwD3kGwrJPbK0ngI=";
+  }
+  {
+    repo = "openthread";
+    path = "modules/lib/openthread";
+    rev = "b21e99b4b3d823f71c902b9174ff62b964c124f0";
+    sha256 = "CQGzxISYncLT6hXREcBP30gqXOlaq7V6ltJXcOP+tc4=";
+  }
+  {
+    repo = "picolibc";
+    path = "modules/lib/picolibc";
+    rev = "04ada5951cbaf8e7b17f8226ce31cb6837c28ba7";
+    sha256 = "sKgN85PaVzqxwZEbgzD9JY8pD3bxLRCG9ocxt8KGZms=";
+  }
+  {
+    repo = "segger";
+    path = "modules/debug/segger";
+    rev = "d4e568a920b4bd087886170a5624c167b2d0665e";
+    sha256 = "2IKMMZE+29Ze4DliM61XUcYqPCMqhkvh/MIGKhaYOSg=";
   }
   {
     repo = "sof";
     path = "modules/audio/sof";
-    rev = "b5b772dd611879ee6309da38759def79fc7f33f1";
-    sha256 = "iOabokoONU7JGi3vFeKFQ79A3kkK7SQAEquHRw2nL64=";
+    rev = "fed466c264ad078c66f7bff9218ba1d3fa0eb201";
+    sha256 = "GDLtSBT8oU9Uz43UmOX47ysiA347ITZMvTOKx4XYzaI=";
+  }
+  {
+    repo = "tflite-micro";
+    path = "modules/lib/tflite-micro";
+    rev = "9156d050927012da87079064db59d07f03b8baf6";
+    sha256 = "Aa5mFsGFc3c7F7GleL0QfB5XBl1ihYGsGIDCnEPBP98=";
+    # repo-path: tflite-micro
   }
   {
     repo = "tinycbor";
     path = "modules/lib/tinycbor";
-    rev = "40daca97b478989884bffb5226e9ab73ca54b8c4";
-    sha256 = "MbJbXqX+CJMQ0ctGDDGG4nOvX6IM6CB1srE452SOQAs=";
+    rev = "9e1f34bc08123aaad7666d3652aaa839e8178b3b";
+    sha256 = "bycKC0g04sDkj2WoBYHKJU371VsXY9oQZzFgTk0uf1Y=";
   }
   {
     repo = "tinycrypt";
@@ -131,21 +296,51 @@
     sha256 = "5gtZbZNx+D/EUkyYk7rPtcxBZaNs4IFGTP/7IXzCoqU=";
   }
   {
-    repo = "littlefs";
-    path = "modules/fs/littlefs";
-    rev = "9e4498d1c73009acd84bb36036ee5e2869112a6c";
-    sha256 = "lN4z5whLmaLWpQiY1OPq3p7rBCWJZUO9Kv2R6g+O48A=";
+    repo = "TraceRecorderSource";
+    path = "modules/debug/TraceRecorder";
+    rev = "9893bf1cf649a2c4ee2e27293f887994f3d0da5b";
+    sha256 = "jzox0g/M2nKSRGICjl1AX5iruH58RoGnsRQfOzCGjwI=";
   }
   {
-    repo = "mipi-sys-t";
-    path = "modules/debug/mipi-sys-t";
-    rev = "75e671550ac1acb502f315fe4952514dc73f7bfb";
-    sha256 = "fi7jL2vDmZyEoyO69Q4YOQnwZsKHJ4AxRgENnicHy2w=";
+    repo = "trusted-firmware-m";
+    path = "modules/tee/tf-m/trusted-firmware-m";
+    rev = "231235f26f5295ac4faf8c5617dbb9779869d821";
+    sha256 = "1u2OiMw+JAtKJYvk7xSi4ENP5k5U9ehj8FECmvJ4CZo=";
   }
   {
-    repo = "nrf_hw_models";
-    path = "modules/bsim_hw_models/nrf_hw_models";
-    rev = "a47e326ca772ddd14cc3b9d4ca30a9ab44ecca16";
-    sha256 = "tDpst18bnjnTHNsB6KDYINYe0WJGry8lYBB2m2LZRQA=";
+    repo = "trusted-firmware-a";
+    path = "modules/tee/tf-a/trusted-firmware-a";
+    rev = "d29cddecde614d81cbec1fb0086cdaebd77d3575";
+    sha256 = "yWTGyTPx8ykpPCJQdYi7AVS2Pnr3dvko0o2n3sVBivU=";
+  }
+  {
+    repo = "tf-m-tests";
+    path = "modules/tee/tf-m/tf-m-tests";
+    rev = "c99a86b295c4887520da9d8402566d7f225c974e";
+    sha256 = "ZnQcRp/pHNbQIkQZ9gPOyakXpRc0eXo3wd/hFJMAhDE=";
+  }
+  {
+    repo = "psa-arch-tests";
+    path = "modules/tee/tf-m/psa-arch-tests";
+    rev = "f4fc2442b8e29e2a03d9899e46e5a3ea3df8c2c9";
+    sha256 = "XrnII9x2bnedDYyXZ80SoHce5k9i4ctpYM5jh41VuAQ=";
+  }
+  {
+    repo = "uoscore-uedhoc";
+    path = "modules/lib/uoscore-uedhoc";
+    rev = "e8920192b66db4f909eb9cd3f155d5245c1ae825";
+    sha256 = "pJ5I08qigr1hC8LnocEAlZGgb4P0+gxyvSiG6UoayZo=";
+  }
+  {
+    repo = "zcbor";
+    path = "modules/lib/zcbor";
+    rev = "a0d6981f14d4001d6f0d608d1a427f9bc6bb6d02";
+    sha256 = "rHELdE3lOSXmL9sORedxyNQUUfYs6VmKtxMEEW86vw8=";
+  }
+  {
+    repo = "zscilib";
+    path = "modules/lib/zscilib";
+    rev = "ca070ddabdaf67175a2da901d0bd62e8899371c5";
+    sha256 = "e8BvHqzHblg1RcoOLlWSNIGeQPgrWubGQv938R0dQ+s=";
   }
 ]
